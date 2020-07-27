2f91a37c3ad760b079f999bcfe04d76a23c736f4
Vitor A <66949762+vitorsergiota@users.noreply.github.com>
2020-07-27 18:11:55 +0100

    Js DOM
    Aprender o que é o DOM document object model

---
+++ dom/scripts/main.js
@@ -0,0 +1,11 @@
+var p1 = window.document.getElementsByTagName(`p`)[0];
+var fundo = window.document.getElementsByName(`fundo`)[0];
+window.document.write(`hello world`);
+fundo.style.color = `blue`;
+fundo.style.background = `lightblue`;
+
+
+
+
+
+
