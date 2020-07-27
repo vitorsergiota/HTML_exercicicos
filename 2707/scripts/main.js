8098487aa6185c14fc2fcbb5dcdd0fc3093f68f8
Vitor A <66949762+vitorsergiota@users.noreply.github.com>
2020-07-27 22:51:38 +0100

    Criar evento
    criação de um evento com mouse

---
+++ 2707/scripts/main.js
@@ -0,0 +1,12 @@
+function clicar() {
+    var a = window.document.getElementById=("area")
+    a.innerText = `Clicou!`
+}
+function entrar() {
+    var a = window.document.getElementById=("area") 
+    a.innerText = `Entrou!`
+}
+function sair() {
+    var a = window.document.getElementById=(`area`) 
+    a.innerText = `Adeus!`
+}
--- dom/scripts/main.js
+++ dom/scripts/main.js
@@ -1,11 +1,4 @@
 var p1 = window.document.getElementsByTagName(`p`)[0];
 var fundo = window.document.getElementsByName(`fundo`)[0];
-window.document.write(`hello world`);
 fundo.style.color = `blue`;
 fundo.style.background = `lightblue`;
-
-
-
-
-
-
