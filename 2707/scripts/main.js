1b8a7724d51f1ebee807bd367db0c10a9eaa2118
Vitor A <66949762+vitorsergiota@users.noreply.github.com>
2020-07-28 21:14:26 +0100

    Trabalhando DOM
    criando Eventos

--- 2707/scripts/main.js
+++ 2707/scripts/main.js
@@ -1,12 +1,33 @@
-function clicar() {
-    var a = window.document.getElementById=("area")
-    a.innerText = `Clicou!`
-}
-function entrar() {
-    var a = window.document.getElementById=("area") 
-    a.innerText = `Entrou!`
-}
-function sair() {
-    var a = window.document.getElementById=(`area`) 
-    a.innerText = `Adeus!`
-}
+var a = window.document.getElementById("area")
+    function clicar() {
+        a.innerText ="Clicou!"
+        a.style.background ="red"
+    }
+    function entrar() {
+        a.innerText ="Entrou!"
+    }
+    function sair() {
+        a.innerText ="Saiu!"
+        a.style.background ="green"
+    }
+    
+    // segundo elemento mas somente com JS
+
+var b = window.document.getElementById("area2")
+    b.addEventListener("click", clicar2 )
+    b.addEventListener("mouseenter", entrar2 )
+    b.addEventListener("mouseout", sair2 )
+    function clicar2() {
+        b.innerText ="Clicou!"
+        b.style.color = "black"
+        b.style.background ="yellow"
+    }
+    function entrar2() {
+        b.innerText = "Entrou"
+        b.style.color = "white"
+    }
+    function sair2() {
+        b.innerText = "Saiu!"
+        b.style.background = "blue"
+        b.style.color = "white"
+    }
