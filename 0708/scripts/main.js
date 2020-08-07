0639f2845b94ed94bb5a0567ddc1c4d63bc8d62b
Vitor A <66949762+vitorsergiota@users.noreply.github.com>
2020-08-07 22:13:32 +0100

    Algoritmos
    Retifiquei erros para ficar a funcionar

--- 0708/scripts/main.js
+++ 0708/scripts/main.js
@@ -1,17 +1,16 @@
 function calcular() {
-    var lada = window.document.querySelector(`input#tr01`)
-    var ladb = window.document.querySelector(`input#tr02`)
-    var ladc = window.document.querySelector(`input#tr03`)
+    var lada = window.document.querySelector(`#tri01`)
+    var ladb = window.document.querySelector(`#tri02`)
+    var ladc = window.document.querySelector(`#tri03`)
     var res = window.document.querySelector(`#res`)
     var lado1 = Number(lada.value)
     var lado2 = Number(ladb.value)
     var lado3 = Number(ladc.value)
     if ( lado1 == lado2 && lado2 == lado3) {
-        res.innerHTML(`Este triângulo é Equilatero`)
+        res.innerHTML = `Este triângulo é Equilatero`
     } else if ( lado1 != lado2 && lado2 != lado3 && lado3 != lado1) {
-        res.innerHTML(`Este triângulo é Escaleno`)
+        res.innerHTML = `Este triângulo é Escaleno`
     } else {
-        res.innerHTML(`Este triângulo é Isósceles`)
+        res.innerHTML = `Este triângulo é Isósceles`
     }
 }
-res.innerHTML(lado1)
