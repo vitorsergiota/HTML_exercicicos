ca447d7d8a2c0e45f78a26d561877fc3e52984a1
Vitor A <66949762+vitorsergiota@users.noreply.github.com>
2020-08-14 18:01:06 +0100

    Pus a funcionar

--- 1408/scripts/main.js
+++ 1408/scripts/main.js
@@ -1,16 +1,16 @@
 function verdade() {
     var lindo = document.getElementsByName(`cara`)
     var res = document.querySelector(`#res`)
-    var ima = document.createElement(`img`)
-    ima = setAttribute(`id`, `foto`)
+    var img = document.createElement(`img`)
     if (lindo[0].checked) {
-        ima.setAttribute(`src`, `imagens/lindo.jpg`)
+        img.setAttribute(`src`, `imagens/lindo.jpg`)
         res.innerHTML = `És lindoooo!`
     } else if (lindo[1].checked) {
-        ima.setAttribute(`src`, `imagens/feio.jpg`)
+        img.setAttribute(`src`, `imagens/feio.jpg`)
         res.innerHTML = `Que jeitoso tu és!`
     }else {
-        ima.setAttribute(`src`, `imagens/sol.jpg`)
+        img.setAttribute(`src`, `imagens/sol.jpg`)
         res.innerHTML = `WoooooWWWWW....`
     }
+    res.appendChild(img)
 }
