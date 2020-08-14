5d5f25945a7fe9180190432ba89202e0defc14b9
Vitor A <66949762+vitorsergiota@users.noreply.github.com>
2020-08-14 16:22:20 +0100

    Funcional
    coloquei a funcionar.

--- 1308/scripts/main.js
+++ 1308/scripts/main.js
@@ -1,6 +1,11 @@
 function verificar() {
     var raca = document.getElementsByName(`race`)
     var res = document.querySelector(`#res`)
-    if (race.checked[0])
-
+    if (raca[0].checked) {
+        res.innerHTML = `Tu és um Alienigena!`
+    } else if (raca[1].checked) {
+        res.innerHTML = `Seu Humano mizarento!`
+    } else {
+        res.innerHTML = `Quem és tu???`
+    }
 }
