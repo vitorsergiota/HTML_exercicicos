0168cf842c9b8337ad8c92d6ba8cf611bb5f79db
Vitor A <66949762+vitorsergiota@users.noreply.github.com>
2020-08-14 18:21:45 +0100

    testei outra função
    sem sucesso juntava mais imagens

--- 1408/scripts/main.js
+++ 1408/scripts/main.js
@@ -1,16 +1,19 @@
 function verdade() {
+    
     var lindo = document.getElementsByName(`cara`)
     var res = document.querySelector(`#res`)
-    var img = document.createElement(`img`)
+    
+    // criação elemento img 
+    var ima = document.createElement(`img`)
     if (lindo[0].checked) {
-        img.setAttribute(`src`, `imagens/lindo.jpg`)
+        ima.setAttribute(`src`, `imagens/lindo.jpg`)
         res.innerHTML = `És lindoooo!`
     } else if (lindo[1].checked) {
-        img.setAttribute(`src`, `imagens/feio.jpg`)
+        ima.setAttribute(`src`, `imagens/feio.jpg`)
         res.innerHTML = `Que jeitoso tu és!`
     }else {
-        img.setAttribute(`src`, `imagens/sol.jpg`)
+        ima.setAttribute(`src`, `imagens/sol.jpg`)
         res.innerHTML = `WoooooWWWWW....`
     }
-    res.appendChild(img)
+    res.appendChild(ima)
 }
