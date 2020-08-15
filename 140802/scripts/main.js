68a6dc5b4ad656956087452b5f563faeb99aad18
Vitor A <66949762+vitorsergiota@users.noreply.github.com>
2020-08-15 01:10:16 +0100

    Mudar imagens
    Mudar imagens com Html e Js

---
+++ 140802/scripts/main.js
@@ -0,0 +1,8 @@
+function mudar() {
+    var img = document.querySelector(`#img1`)
+    img.src = `imagens/imagem2.jpg`
+}
+function voltar() {
+    var img = document.querySelector(`#img1`)
+    img.src = `imagens/imagem1.jpg`
+}
