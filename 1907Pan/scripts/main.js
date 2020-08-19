5d241e58477090f9afa1e411c37b38dcff6a51a2
Vitor A <66949762+vitorsergiota@users.noreply.github.com>
2020-08-19 18:16:12 +0100

    Troca de foto
    Alterei codigo para mudar foto

--- 1907Pan/scripts/main.js
+++ 1907Pan/scripts/main.js
@@ -6,3 +6,16 @@ function voltar() {
     var ima = document.getElementById(`imo`)
     ima.src = `images/gray.jpg`
 }
+function clica() {
+    var img = document.getElementById(`imo`)
+    img.src = `images/electra.jpg`
+}
+function teste() {
+    let imgteste = document.getElementById(`teste`)
+    let meusrc = imgteste.getAttribute(`src`)
+    if (meusrc === `images/blue.jpg`) {
+        imgteste.setAttribute (`src`, `images/black.jpg`)
+    } else {
+        imgteste.setAttribute (`src`, `images/blue.jpg`)
+    }
+}
