10a223ced1afc66c350e786763603a34b403d0ec
Vitor A <66949762+vitorsergiota@users.noreply.github.com>
2020-08-20 17:45:07 +0100

    Clica troca img
    coloquei as 3 fotos a trocar

--- 1907Pan/scripts/main.js
+++ 1907Pan/scripts/main.js
@@ -10,12 +10,30 @@ function clica() {
     var img = document.getElementById(`imo`)
     img.src = `images/electra.jpg`
 }
-function teste() {
-    let imgteste = document.getElementById(`teste`)
+function troca1() {
+    let imgteste = document.getElementById(`im1`)
     let meusrc = imgteste.getAttribute(`src`)
     if (meusrc === `images/blue.jpg`) {
         imgteste.setAttribute (`src`, `images/black.jpg`)
     } else {
         imgteste.setAttribute (`src`, `images/blue.jpg`)
+    }
+}
+function troca2() {
+    let img2 = document.getElementById(`im2`)
+    let meusrc2 = img2.getAttribute(`src`)
+    if (meusrc2 === `images/black.jpg`) {
+        img2.setAttribute (`src`, `images/blue.jpg`)
+    } else {
+        img2.setAttribute (`src`, `images/black.jpg`)
+    }
+}
+function troca3() {
+    let img3 = document.getElementById(`im3`)
+    let meusrc3 = img3.getAttribute(`src`)
+    if (meusrc3 === `images/red.jpg`) {
+        img3.setAttribute (`src`, `images/blue.jpg`)
+    } else {
+        img3.setAttribute (`src`, `images/red.jpg`)
     }
 }
